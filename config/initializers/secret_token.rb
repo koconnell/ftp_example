c67889d1b4399689408bd3999f7540e31abc7f91
Kevin O'Connell <koconnell@connamara.com>
2012-07-19 09:22:13 -0500

    project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FtpExample::Application.config.secret_token = '8f907c157101593a5293a350a76d1a4cba348c362bc4f75c6687ceed8984ff524cd268a26b304014c2e42538b49e9d37bcccd84521c77955a97f18ec81e339e1'
